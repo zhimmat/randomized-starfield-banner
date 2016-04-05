07fbe54ea8722dee4694120409f1099709eb0026
zhimmat <zeyad.himmat@live.com>
2016-04-05 14:37:44 -0400

    added more code in js

--- js/main.js
+++ js/main.js
@@ -2,3 +2,10 @@ var $body = $('.banner');
 var $star;
 var bannerWidth = $body.outerWidth();
 var bannerHeight = $body.outerHeight();
+
+for (i = 0; i < 75; i++) {
+  $star = $('<div>');
+  $star.addClass('star');
+  $star.css({
+
+  });
