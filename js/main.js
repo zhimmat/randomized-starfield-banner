5778b8a23c6e61399e40a22c99c119d86eb855ed
zhimmat <zeyad.himmat@live.com>
2016-04-05 14:41:57 -0400

    new code in js and css all that's left is keyframes

--- js/main.js
+++ js/main.js
@@ -7,5 +7,8 @@ for (i = 0; i < 75; i++) {
   $star = $('<div>');
   $star.addClass('star');
   $star.css({
-
+    top: Math.random() * bannerHeight,
+    left: Math.random() * bannerWidth,
+    opacity: Math.random(),
+    transform: 'rotate(' + Math.random() * 360 + 'deg) scale(' + Math.random() + ')'
   });
